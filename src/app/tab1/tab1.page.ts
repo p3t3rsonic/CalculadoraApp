bc74cc6ccb152f10b69c2eb41f3ad6a3759c9d91
Pedro Passarin <pedro_passarin@hotmail.com>
2021-08-27 14:38:51 -0300

    testes do para subir o projeto no github

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -7,6 +7,33 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
+  public primeiroValor: number;
+  public segundoValor: number;
+
+  public resultado: number;
+
   constructor() {}
+
+  public soma(){
+    this.resultado = this.primeiroValor + this.segundoValor;
+  }
+
+  public sub(){
+    this.resultado = this.primeiroValor - this.segundoValor;
+  }
+
+  public div(){
+    this.resultado = this.primeiroValor / this.segundoValor;
+  }
+
+  public mult(){
+    this.resultado = this.primeiroValor * this.segundoValor;
+  }
+
+  public limpar(){
+    this.primeiroValor = null;
+    this.segundoValor = null;
+    this.resultado = null;
+  }
 
 }
