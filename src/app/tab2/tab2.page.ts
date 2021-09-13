74612ff0877d5655347cef33dca0981e2f5ba29d
Pedro Passarin <pedro_passarin@hotmail.com>
2021-09-13 18:29:54 -0300

    Alterações na Calc simples

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
+
 export class Tab2Page {
 
-  constructor() {}
+  public calculo = '';
+  public resultado: string;
+
+   constructor(){}
+
+   adicionarNumero(valor: string){
+
+    this.calculo = this.calculo + valor;
+   }
+
+   adicionarPonto(){
+     this.calculo = this.calculo + ".";
+   }
+
+   adicionarOp(op: string){
+     this.calculo = this.calculo + op;
+    }
 
 }
