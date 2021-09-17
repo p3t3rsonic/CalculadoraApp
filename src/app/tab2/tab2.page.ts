44581446094f0465b39be3caa122c4cccba8353f
Pedro Passarin <pedro_passarin@hotmail.com>
2021-09-16 21:28:07 -0300

    mais algumas alteracoes na calc aba 2

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Validators } from '@angular/forms';
+import { evaluate } from 'mathjs';
 
 @Component({
   selector: 'app-tab2',
@@ -12,6 +13,8 @@ export class Tab2Page {
   public calculo = '';
   public resultado: string;
 
+  public ponto = false;
+
    constructor(){}
 
    adicionarNumero(valor: string){
@@ -20,11 +23,38 @@ export class Tab2Page {
    }
 
    adicionarPonto(){
+     if (this.ponto){
+       return;
+     }
      this.calculo = this.calculo + ".";
+     this.ponto = true;
    }
 
    adicionarOp(op: string){
      this.calculo = this.calculo + op;
+    }
+
+    public limparTudo(){
+      this.calculo = '';
+      this.resultado = null;
+      this.ponto = false;
+    }
+
+    public limparUlt(){
+      const ult = this.calculo.slice(-1);
+      if (ult == '.'){
+        this.ponto=false;
+      }
+      this.calculo = this.calculo.slice(0, -1);
+    }
+
+    public calcRes(){
+      try{
+        this.resultado = evaluate(this.calculo);
+      }catch (e){
+        this.resultado = '';
+      }
+
     }
 
 }
